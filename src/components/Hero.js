a97c4e3447b46d27af7449dcd5bb05797f5d810e
JunhoRS <126283654+JunhoRS@users.noreply.github.com>
2023-07-08 23:43:16 -0400

    change some text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
                 <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
                 <h1 class="text-5xl font-bold">Mis proyectos</h1>
-                <p class="py-6">Aprecia todos estos proyectos utilizando diferentes tipos de lenguajes.</p>
+                <p class="py-6">Lista de proyetos creados.</p>
                 <button class="btn btn-primary">EXPLORAR</button>
                 </div>
             </div>
