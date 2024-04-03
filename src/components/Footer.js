1e6abe7dd415757e16a65d62ab566b2fabcd98c8
JunhoRS <126283654+JunhoRS@users.noreply.github.com>
2024-04-03 18:57:50 -0400

    Implementar nueva funcionalidad

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
                 </div>
             </div> 
             <div>
-                <p>Copyright © 2023 - All right reserved by ACME Industries Ltd JunhoRS</p>
+                <p>Copyright © 2023 - All right reserved by JunhoRS</p>
             </div>
         </footer>
     </>
